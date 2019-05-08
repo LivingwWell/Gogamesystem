bddb2437ed9b7ed5acb70cb60b143e312e012b73
LivingwWell <765995471@QQ.com>
2019-05-08 20:00:30 +0800

    5.1

--- app/src/main/java/com/example/gogamesystem/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/gogamesystem/activity/UserInfoActivity.java
@@ -1,22 +1,20 @@
 package com.example.gogamesystem.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.gogamesystem.R;
 import com.example.gogamesystem.bean.Game;
 import com.example.gogamesystem.bean.User;
-import com.example.gogamesystem.fragment.UserInfoFragment;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -44,6 +42,10 @@ public class UserInfoActivity extends AppCompatActivity {
     Button button5;
     @BindView(R.id.info_qifeng)
     TextView infoQifeng;
+    @BindView(R.id.info_game)
+    TextView infoGame;
+    @BindView(R.id.infosex)
+    TextView infosex;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,6 +53,7 @@ public class UserInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_userinfo);
         ButterKnife.bind(this);
         initView();
+
     }
 
     private void initView() {
@@ -66,8 +69,10 @@ public class UserInfoActivity extends AppCompatActivity {
                     infoAge.setText(String.valueOf(list.get(0).getAge()));
                     infoClub.setText(list.get(0).getClub());
                     infoSex.setText(list.get(0).getSex());
-                    infoWin.setText(getwin());
+                    getwin();
                     infoQifeng.setText(getMethod());
+                    getGame();
+                    getSex();
                 } else {
                     Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
                 }
@@ -78,30 +83,55 @@ public class UserInfoActivity extends AppCompatActivity {
     int winnum;
     int defeatnum;
 
-    public String getwin() {
+    //胜率查询
+    public void getwin() {
         BmobQuery<Game> victory = new BmobQuery<Game>();
         victory.addWhereEqualTo("victory", infoUser.getText().toString());
         victory.findObjects(new FindListener<Game>() {
             @Override
             public void done(List<Game> list, BmobException e) {
                 winnum = list.size();
+                BmobQuery<Game> defeat = new BmobQuery<Game>();
+                defeat.addWhereEqualTo("defeat", infoUser.getText().toString());
+                defeat.findObjects(new FindListener<Game>() {
+                    @Override
+                    public void done(List<Game> list, BmobException e) {
+                        defeatnum = list.size();
+                        NumberFormat numberFormat = NumberFormat.getInstance();
+                        numberFormat.setMaximumFractionDigits(2);
+                        String result = numberFormat.format((float) winnum / (float) (winnum + defeatnum) * 100);
+                        Log.d("UserInfoActivity", "winnum: " + winnum + "defeatnum:" + defeatnum);
+                        infoWin.setText(result + "%");
+                    }
+                });
             }
         });
-        BmobQuery<Game> defeat = new BmobQuery<Game>();
-        defeat.addWhereEqualTo("defeat", infoUser.getText().toString());
-        defeat.findObjects(new FindListener<Game>() {
-            @Override
-            public void done(List<Game> list, BmobException e) {
-                defeatnum = list.size();
-            }
-        });
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        numberFormat.setMaximumFractionDigits(2);
-        String result = numberFormat.format((float) winnum / (float) (winnum + defeatnum) * 100);
-        Log.d("getwin", "winnum: " + winnum + "defeatnum:" + defeatnum);
-        return result + "%";
+        //这里你查询了Victory 后还要在查询词defeat 最后才得出结果这样才是正确的,这也是按照逻辑的方法走的,该怎么去包装就得要想一下了
     }
-
+    //查询男女比例
+    public void getSex() {
+      BmobQuery<User> man=new BmobQuery<User>();
+      man.addWhereEqualTo("sex","男");
+      man.findObjects(new FindListener<User>() {
+          @Override
+          public void done(List<User> list, BmobException e) {
+              int mannum=list.size();
+              BmobQuery<User> woman=new BmobQuery<User>();
+              woman.addWhereEqualTo("sex","女");
+              woman.findObjects(new FindListener<User>() {
+                  @Override
+                  public void done(List<User> list, BmobException e) {
+                     int womanum=list.size();
+                      NumberFormat numberFormat = NumberFormat.getInstance();
+                      numberFormat.setMaximumFractionDigits(2);
+                      String result = numberFormat.format((float) mannum / (float) (mannum + womanum) * 100);
+                      Log.d("UserInfoActivity", "winnum: " + mannum + "defeatnum:" + womanum);
+                      infosex.setText("男生为"+result + "%");
+                  }
+              });
+          }
+      });
+    }
     int zhongnum;
     int guannum;
     String method;
@@ -130,9 +160,38 @@ public class UserInfoActivity extends AppCompatActivity {
         return method;
     }
 
+
+    //查询参加过的比赛
+    public void getGame() {
+        StringBuffer buf=new StringBuffer();
+        BmobQuery<Game> eq1=new BmobQuery<>();
+        eq1.addWhereEqualTo("black",infoUser.getText());
+        BmobQuery<Game> eq2=new BmobQuery<>();
+        eq2.addWhereEqualTo("white",infoUser.getText());
+        List<BmobQuery<Game>> andQuerys=new ArrayList<BmobQuery<Game>>();
+        andQuerys.add(eq1);
+        andQuerys.add(eq2);
+        BmobQuery<Game> query=new BmobQuery<>();
+        query.or(andQuerys);
+        query.findObjects(new FindListener<Game>() {
+            @Override
+            public void done(List<Game> list, BmobException e) {
+                if (e==null){
+                    for (int i=0;i<list.size();i++){
+                        Log.i("getGame","参加过的比赛："+ list.get(i).getName());
+                        buf.append(list.get(i).getName()+"\n");
+                        infoGame.setText(buf.toString());
+                    }
+                    Log.i("getGame","buf："+ buf.toString());
+                }else { Log.i("getGame","失败："+e.getMessage()+","+e.getErrorCode());}
+            }
+        });
+    }
+
+
+
     @OnClick(R.id.button5)
     public void onClick() {
-        Intent intent = new Intent(UserInfoActivity.this, LoginAcivity.class);
-        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/gogamesystem/fragment/UserInfoFragment.java
+++ app/src/main/java/com/example/gogamesystem/fragment/UserInfoFragment.java
@@ -19,6 +19,7 @@ import com.example.gogamesystem.bean.Game;
 import com.example.gogamesystem.bean.User;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -30,6 +31,7 @@ import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
 
 import static com.example.gogamesystem.activity.LoginAcivity.user_name;
+import static com.example.gogamesystem.activity.VsInfoActivity.vsname;
 
 public class UserInfoFragment extends Fragment {
     @BindView(R.id.info_user)
@@ -44,12 +46,16 @@ public class UserInfoFragment extends Fragment {
     TextView infoClub;
     @BindView(R.id.info_win)
     TextView infoWin;
-    Unbinder unbinder;
     @BindView(R.id.button5)
     Button button5;
     @BindView(R.id.info_qifeng)
     TextView infoQifeng;
+    @BindView(R.id.info_game)
+    TextView infoGame;
+    @BindView(R.id.infosex)
+    TextView infosex;
 
+    Unbinder unbinder;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -59,9 +65,15 @@ public class UserInfoFragment extends Fragment {
         return view;
     }
 
+    @OnClick(R.id.button5)
+    public void onClick() {
+        Intent intent = new Intent(UserInfoFragment.this.getActivity(), LoginAcivity.class);
+        startActivity(intent);
+    }
+
     private void initView() {
         BmobQuery<User> query = new BmobQuery<User>();
-        query.addWhereEqualTo("username", user_name);
+        query.addWhereEqualTo("username", vsname);
         query.findObjects(new FindListener<User>() {
             @Override
             public void done(List<User> list, BmobException e) {
@@ -72,8 +84,10 @@ public class UserInfoFragment extends Fragment {
                     infoAge.setText(String.valueOf(list.get(0).getAge()));
                     infoClub.setText(list.get(0).getClub());
                     infoSex.setText(list.get(0).getSex());
-                    infoWin.setText(getwin());
+                    getwin();
                     infoQifeng.setText(getMethod());
+                    getGame();
+                    getSex();
                 } else {
                     Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
                 }
@@ -81,49 +95,63 @@ public class UserInfoFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        unbinder.unbind();
-    }
-
-    @OnClick(R.id.button5)
-    public void onClick() {
-        Intent intent = new Intent(UserInfoFragment.this.getActivity(), LoginAcivity.class);
-        startActivity(intent);
-    }
-
     int winnum;
     int defeatnum;
 
-    public String getwin() {
+    //胜率查询
+    public void getwin() {
         BmobQuery<Game> victory = new BmobQuery<Game>();
         victory.addWhereEqualTo("victory", infoUser.getText().toString());
         victory.findObjects(new FindListener<Game>() {
             @Override
             public void done(List<Game> list, BmobException e) {
                 winnum = list.size();
+                BmobQuery<Game> defeat = new BmobQuery<Game>();
+                defeat.addWhereEqualTo("defeat", infoUser.getText().toString());
+                defeat.findObjects(new FindListener<Game>() {
+                    @Override
+                    public void done(List<Game> list, BmobException e) {
+                        defeatnum = list.size();
+                        NumberFormat numberFormat = NumberFormat.getInstance();
+                        numberFormat.setMaximumFractionDigits(2);
+                        String result = numberFormat.format((float) winnum / (float) (winnum + defeatnum) * 100);
+                        Log.d("UserInfoActivity", "winnum: " + winnum + "defeatnum:" + defeatnum);
+                        infoWin.setText(result + "%");
+                    }
+                });
             }
         });
-        BmobQuery<Game> defeat = new BmobQuery<Game>();
-        defeat.addWhereEqualTo("defeat", infoUser.getText().toString());
-        defeat.findObjects(new FindListener<Game>() {
+        //这里你查询了Victory 后还要在查询词defeat 最后才得出结果这样才是正确的,这也是按照逻辑的方法走的,该怎么去包装就得要想一下了
+    }
+    //查询男女比例
+    public void getSex() {
+        BmobQuery<User> man=new BmobQuery<User>();
+        man.addWhereEqualTo("sex","男");
+        man.findObjects(new FindListener<User>() {
             @Override
-            public void done(List<Game> list, BmobException e) {
-                defeatnum = list.size();
+            public void done(List<User> list, BmobException e) {
+                int mannum=list.size();
+                BmobQuery<User> woman=new BmobQuery<User>();
+                woman.addWhereEqualTo("sex","女");
+                woman.findObjects(new FindListener<User>() {
+                    @Override
+                    public void done(List<User> list, BmobException e) {
+                        int womanum=list.size();
+                        NumberFormat numberFormat = NumberFormat.getInstance();
+                        numberFormat.setMaximumFractionDigits(2);
+                        String result = numberFormat.format((float) mannum / (float) (mannum + womanum) * 100);
+                        Log.d("UserInfoActivity", "winnum: " + mannum + "defeatnum:" + womanum);
+                        infosex.setText("男生为"+result + "%");
+                    }
+                });
             }
         });
-        NumberFormat numberFormat = NumberFormat.getInstance();
-        numberFormat.setMaximumFractionDigits(2);
-        String result = numberFormat.format((float) winnum / (float) (winnum + defeatnum) * 100);
-        Log.d("getwin", "winnum: " + winnum + "defeatnum:" + defeatnum);
-        return result + "%";
     }
 
     int zhongnum;
     int guannum;
     String method;
-
+    //棋风查询
     public String getMethod() {
         BmobQuery<Game> zhong = new BmobQuery<Game>();
         zhong.addWhereEqualTo("victory", infoUser.getText().toString());
@@ -146,5 +174,41 @@ public class UserInfoFragment extends Fragment {
             method = "关子胜";
         }
         return method;
+    }
+
+
+    //查询参加过的比赛
+    public void getGame() {
+        StringBuffer buf=new StringBuffer();
+        BmobQuery<Game> eq1=new BmobQuery<>();
+        eq1.addWhereEqualTo("black",infoUser.getText());
+        BmobQuery<Game> eq2=new BmobQuery<>();
+        eq2.addWhereEqualTo("white",infoUser.getText());
+        List<BmobQuery<Game>> andQuerys=new ArrayList<BmobQuery<Game>>();
+        andQuerys.add(eq1);
+        andQuerys.add(eq2);
+        BmobQuery<Game> query=new BmobQuery<>();
+        query.or(andQuerys);
+        query.findObjects(new FindListener<Game>() {
+            @Override
+            public void done(List<Game> list, BmobException e) {
+                if (e==null){
+                    for (int i=0;i<list.size();i++){
+                        Log.i("getGame","参加过的比赛："+ list.get(i).getName());
+                        buf.append(list.get(i).getName()+"\n");
+                        infoGame.setText(buf.toString());
+                    }
+                    Log.i("getGame","buf："+ buf.toString());
+                }else { Log.i("getGame","失败："+e.getMessage()+","+e.getErrorCode());}
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
